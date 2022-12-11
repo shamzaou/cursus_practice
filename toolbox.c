d8c7772bb9d23d587b09e9ca2b43c2cbf287d747
shamzaou <shamzaou@student.42abudhabi.ae>
2022-12-11 07:34:54 +0400

    update

--- toolbox.c
+++ toolbox.c
@@ -7,3 +7,22 @@ void    print_char(va_list ap, int *len_ptr)
     c = va_arg(ap, char);
     *len_ptr += write(1, &c, 1);
 }
+
+void    print_str(va_list ap, int *len_ptr)
+{
+    char    *str;
+    int     i;
+
+    str = va_arg(ap, char *);
+    i = 0;
+    while (str[i])
+    {
+        *len_ptr += write(1, &str[i], 1);
+        i++;
+    }
+}
+
+void    print_ptr(va_list ap, int *len_ptr)
+{
+    
+}
